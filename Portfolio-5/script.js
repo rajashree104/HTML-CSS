35fdd331d2057f5541ac02938195dde6c7b0791d
rajashree104 <rajashreesahoo944@gmail.com>
2025-11-07 21:00:43 +0530

    Project-5  end

---
+++ Portfolio-5/script.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector("#menu");
+const nav = document.querySelector(".links");
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('active');
+}
+
+
+
+
+
